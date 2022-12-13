dd5a2f09f9f5d03c5f2ae278f7d0ee99255e1c48
SadafAlmaas <dsadaf72323@gmail.com>
2022-12-13 19:01:33 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.models import *
+from django.db.models.functions import Length
 
 def display_topics(request):
     LTO=Topic.objects.all()
@@ -11,6 +12,13 @@ def display_topics(request):
 
 def display_webpage(request):
     LWO=Webpage.objects.all()
+    LWO=Webpage.objects.all().order_by('name')
+    LWO=Webpage.objects.all().order_by('-name')
+    LWO=Webpage.objects.filter(topic_name='Cricket')
+    LWO=Webpage.objects.exclude(topic_name='Cricket')
+    LWO=Webpage.objects.all()[:3:]
+    LWO=Webpage.objects.all().order_by(Length('name'))
+    LWO=Webpage.objects.all().order_by(Length('name').desc())
     d={'LWO':LWO}
     return render(request,'display_webpage.html',d)    
 
